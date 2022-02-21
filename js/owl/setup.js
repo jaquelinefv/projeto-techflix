c0bb18eb8a09109e8c1e23913d6197672488cebb
jaquelinefv <jaquelinef.vieira@outlook.com>
2022-02-20 21:19:08 -0300

    Adiciona repositório

---
+++ js/owl/setup.js
@@ -0,0 +1,48 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+//Botões das páginas
+
+//Botão index.html
+
+//Redireciona para a página e resumo
+
+//Informações 
+function maisInformacoes(){
+    window.location.href = "informacoes.html"
+}
+
+
+//Botão eventos.html 
+function maisEventos(){
+    window.location.href = "https://www.showmetech.com.br/calendario-tech-feiras-e-eventos-do-ano/"
+}
+
+
+
+
+//Botão cursos.html/eventos.html/filmes.html
+//Redireciona para a página inicial
+function paginaInicial(){
+    window.location.href = "index.html"
+}
+
+
+
+
+
+
